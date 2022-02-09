ecc2856a8a15d3b4ead7bc68907984f7b1213a1f
eliksheng <eliksheng@tencent.com>
2022-02-09 11:26:35 +0800

    add react test

---
+++ ReactLearning/eth-todo-list-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Web3 from 'web3'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
